e5ff7502be1a721e54611a1fe72948f940d453f4
annusshka <telegina.ann2003@yandex.ru>
2024-03-10 15:37:39 +0300

    Initial commit

---
+++ src/test/java/com/example/FarmSystem/FarmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FarmSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
